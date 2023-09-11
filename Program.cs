3776fdd90578858ab5160a6715d404f6cd0377cf
crewsbd <bdcrews@protonmail.com>
2023-09-11 12:10:15 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿//A simple hello world program with an scrolling animation
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        //Initialize states
+        bool continue_loop = true;
+        int currentX = 2;
+        int minX = 1;
+        int maxX = 30;
+        int speed = 1;
+
+        //Clear the screen and hide the cursor
+        Console.Clear();
+        Console.CursorVisible = false;
+
+        //Animation loop
+        do
+        {
+            //Position the cursor, clear the screen and write the text
+            Console.SetCursorPosition(0, 0);
+            Console.Clear();
+            Console.SetCursorPosition(currentX, 10);
+            if (Console.KeyAvailable) //Stop on any key press
+            {
+                continue_loop = false;
+            }
+
+            Console.Write("Hello World!"); //The main event!
+
+            if (currentX >= maxX) //If extent reached, reverse direction
+            {
+                speed = -speed;
+                currentX = maxX;
+            }
+            else if (currentX <= minX)
+            {
+                speed = -speed;
+                currentX = minX;
+            }
+            currentX += speed; //Move the text
+
+            Thread.Sleep(66); //Delay so we can see the animation
+
+        } while (continue_loop);
+        //Clean
+        Console.Clear();
+        Console.CursorVisible = true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# Hello World
+
+## Overview
+
+This is a simple "Hello World" program written in C#.
+
+The purpose for creating this is to re-familiarize myself with C# and get my development environment in order.
+
+[Software Demo Video](https://youtu.be/bNdCsAs5ImU)
+
+## Development Environment
+
+For this project I used Visual Studio Code with the C# Dev Kit, C# and IntelliCode for C# Dev Kit extensions.
+
+I am using the C# language, an object oriented, cross platform C derivative language created by Microsoft.
+
+## Useful Websites
+
+Some websites that might be useful for creating a "Hello World" program in C# are:
+
+* [Visual Studio Docs](https://code.visualstudio.com/docs/csharp/get-started)
+* [Geeks for Geeks](https://www.geeksforgeeks.org/hello-world-in-c-sharp/)
